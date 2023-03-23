02b845c4e4e56ddad29eb8d9f434eec65eb258ba
DouglasDuarte07 <douglashmduarte123@gmail.com>
2023-03-23 15:52:14 -0300

    sonarqube

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function potentiation(a, b) {
+  return a ** b;
+}
+
+module.exports = { sum, subtraction, division, multiplication, potentiation };
